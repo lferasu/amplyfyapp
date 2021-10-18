d772703aa6a086b254c8e4e159be95ec1694e750
Surafel Teshome Mamo <sura@Surafels-MBP.fios-router.home>
2021-10-17 22:20:48 -0400

    remove about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,14 +7,9 @@ const About = () => (
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
       recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
       voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
+      placeat, accusamus aut saepe..
     </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
-      voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
-    </p>
+    
   </Main>
 );
 
